5232bfff68b3e3d8bae91c09d58829950c0260bd
IlyaShpilko <61794412+IlyaShpilko@users.noreply.github.com>
2021-02-03 19:02:56 +0400

    adding finished code

---
+++ FirstAppTabBar/Screens/TabBar/SecondViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SecondViewController.swift
+//  FirstAppTabBar
+//
+//  Created by Shpilko Ilya on 2/2/21.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func loadView() {
+        super.loadView()
+        print("loadView")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        print("viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear")
+    }
+
+}
